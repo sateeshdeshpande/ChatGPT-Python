852a1ad0a577b30bb86940535eec0975814d7fec
sateeshdeshpande <72216949+sateeshdeshpande@users.noreply.github.com>
2023-10-12 18:35:15 +0100

    Call ChatGPT thru Python Code (Simple)

---
+++ chat.py
@@ -0,0 +1,11 @@
+import os
+import openai
+
+openai.api_key = 'Sateesh- KEY'
+/*
+model_engine = "gpt-3.5-turbo"
+prompt = "Hello, How are you doing today?"
+
+completion = openai.Completion.create(engine=model_engine, prompt=prompt, max_tokens=1024, n=1, stop=None, temperature=0.5)
+response = completion.choices[0].text
+print(response) */
